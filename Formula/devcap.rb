03adfa15a9b4fc360c87c07ba3e16d7ed68004b6
Konrad Michalik <4558190+konradmichalik@users.noreply.github.com>
2026-02-25 12:53:47 +0100

    chore: add devcap formula (renamed from worklog-git)

---
+++ Formula/devcap.rb
@@ -0,0 +1,24 @@
+class Devcap < Formula
+  desc "Aggregate git commits across repos for standups and time tracking"
+  homepage "https://github.com/konradmichalik/devcap"
+  version "0.2.0"
+  license "MIT"
+
+  on_arm do
+    url "https://github.com/konradmichalik/devcap/releases/download/v#{version}/devcap-arm64-apple-darwin.tar.gz"
+    sha256 "44a4beeb3adb5bfa1bf2d09dcc3c4e2fc141b571a49831dc184432c8251f2ad6"
+  end
+
+  on_intel do
+    url "https://github.com/konradmichalik/devcap/releases/download/v#{version}/devcap-x86_64-apple-darwin.tar.gz"
+    sha256 "98ad2096e3573430e24dc33ba362352935383b90d9718404744a63c82dbbf201"
+  end
+
+  def install
+    bin.install "devcap"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/devcap --version")
+  end
+end
