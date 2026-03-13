ffad8f60f4276a79b63480061fa7f5a87ecc943a
Konrad Michalik <4558190+konradmichalik@users.noreply.github.com>
2026-03-13 14:58:52 +0100

    chore: add beacon cask

---
+++ Casks/beacon.rb
@@ -0,0 +1,29 @@
+cask "beacon" do
+  version "0.1.0,v0.1.0"
+
+  on_arm do
+    url "https://github.com/konradmichalik/beacon/releases/download/#{version.after_comma}/Beacon_#{version.before_comma}_aarch64.dmg"
+    sha256 "PLACEHOLDER"
+  end
+
+  on_intel do
+    url "https://github.com/konradmichalik/beacon/releases/download/#{version.after_comma}/Beacon_#{version.before_comma}_x64.dmg"
+    sha256 "PLACEHOLDER"
+  end
+
+  name "Beacon"
+  desc "GitHub & GitLab notification hub for your macOS menu bar"
+  homepage "https://github.com/konradmichalik/beacon"
+
+  app "Beacon.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/Beacon.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/com.beacon.notifications",
+    "~/Library/Caches/com.beacon.notifications",
+  ]
+end
